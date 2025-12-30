ad0a76d05d8b8095bc95c5baa2b4b0fc716d15e8
Tu Nombre <tu_email@ejemplo.com>
2025-12-30 20:56:52 -0300

    cuarto commit

--- ci_integracion.py
+++ ci_integracion.py
@@ -46,6 +46,8 @@ def ir_a_PIM(driver):
     print("✅ Employee Information cargado")
 
 
+
+
 def buscar_id(driver, id="0312"):
     campo_id = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located(
@@ -64,7 +66,7 @@ def buscar_id(driver, id="0312"):
         EC.presence_of_element_located((By.XPATH, '//div[@class="oxd-table-body"]'))
     )
 
-    #print("✅ Resultados de búsqueda cargados OK")
+    print("✅ Resultados de búsqueda cargados OK")
 
 
 # 🔥 TEST PARA GITHUB ACTIONS
